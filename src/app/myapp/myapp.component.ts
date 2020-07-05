e39211c92a91141f4f4fea43872159573c81d9d8
Ramanarayanan Unnikrishnan <ramanarayananunnikrishnan@gmail.com>
2020-07-05 17:30:22 +0530

    First draft - additional

---
+++ src/app/myapp/myapp.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-myapp',
+  templateUrl: './myapp.component.html',
+  styleUrls: ['./myapp.component.css']
+})
+
+export class MyappComponent implements OnInit {  
+  firstname:string;
+  peoplelist:Object[];
+  bindingtext:string;
+
+  constructor() {         
+    this.bindingtext = "#FFFFFF";
+    this.firstname = "Ram";
+    this.peoplelist = [
+      {
+        firstname:"Steve",
+        company:"Apple",
+        hide:true
+      },
+      {
+        firstname:"Elon",
+        company:"Tesla",
+        hide:true
+      },
+      {
+        firstname:"Mark",
+        company:"Facebook",
+        hide:true
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
