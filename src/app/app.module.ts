12659a8f0ca2e2d90da764d84405e0187c15b92a
Ramanarayanan Unnikrishnan <ramanarayananunnikrishnan@gmail.com>
2020-07-05 20:46:24 +0530

    Implemented attribute directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyappComponent } from './myapp/myapp.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyappComponent
+    MyappComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
